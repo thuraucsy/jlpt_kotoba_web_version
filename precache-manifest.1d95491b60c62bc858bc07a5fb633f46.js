7d1d2be7c2df21ef8d420ae2fe144bed9da12721
Thura Aung <thura.ucsy@gmail.com>
2021-03-11 13:01:59 +0900

    first commit

---
+++ precache-manifest.1d95491b60c62bc858bc07a5fb633f46.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30a1dfb6ac7bf3fcb6ae",
+    "url": "/jlpt_kotoba_web_version/css/app.cc138265.css"
+  },
+  {
+    "revision": "09d8f73f4d10d4347799",
+    "url": "/jlpt_kotoba_web_version/css/chunk-vendors.7410142f.css"
+  },
+  {
+    "revision": "3abdda96b0894ed7ae9ba472f138390e",
+    "url": "/jlpt_kotoba_web_version/img/1.3abdda96.jpg"
+  },
+  {
+    "revision": "7839c09776f7f0b9e22bea753ce4a499",
+    "url": "/jlpt_kotoba_web_version/img/2.7839c097.jpg"
+  },
+  {
+    "revision": "5b686205645b3f387ae0e329c1156cd2",
+    "url": "/jlpt_kotoba_web_version/img/3.5b686205.jpg"
+  },
+  {
+    "revision": "b7f8e0e411bc30bdccc70f84f2ae7755",
+    "url": "/jlpt_kotoba_web_version/img/4.b7f8e0e4.jpg"
+  },
+  {
+    "revision": "6448507ef14e3bbad3d5938092ca1675",
+    "url": "/jlpt_kotoba_web_version/img/5.6448507e.jpg"
+  },
+  {
+    "revision": "6636d9536329f2995a4897dd891e5eb8",
+    "url": "/jlpt_kotoba_web_version/img/logo.6636d953.png"
+  },
+  {
+    "revision": "99a384a14f914b5e89e06f2100578420",
+    "url": "/jlpt_kotoba_web_version/index.html"
+  },
+  {
+    "revision": "af4415df8a96e3abcecf",
+    "url": "/jlpt_kotoba_web_version/js/about.0dcee12c.js"
+  },
+  {
+    "revision": "30a1dfb6ac7bf3fcb6ae",
+    "url": "/jlpt_kotoba_web_version/js/app.0af6a0a5.js"
+  },
+  {
+    "revision": "09d8f73f4d10d4347799",
+    "url": "/jlpt_kotoba_web_version/js/chunk-vendors.d8758667.js"
+  },
+  {
+    "revision": "91032e92fd14752be1a82807e25cbfb4",
+    "url": "/jlpt_kotoba_web_version/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jlpt_kotoba_web_version/robots.txt"
+  }
+]);
